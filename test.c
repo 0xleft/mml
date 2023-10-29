653ab87c16b6302d13e6b954cf040aafd95b18ce
plusleft <107749872+0xleft@users.noreply.github.com>
2023-10-29 10:26:16 +0000

    add color to tests

--- test.c
+++ test.c
@@ -5,6 +5,9 @@
 #include "test.h"
 #include "nn.h"
 
+#define RED "\033[0;31m"
+#define RESET "\033[0m"
+
 void test_dataset_functions() {
     printf("test_dataset_functions\n");
     Dataset *dataset = create_dataset(2);
@@ -42,42 +45,42 @@ void test_matrix_functions() {
 
     Matrix *c = dot(a, b);
     if (c->data[0][0] != 19 || c->data[0][1] != 22 || c->data[1][0] != 43 || c->data[1][1] != 50) {
-        printf("dot(a, b) failed\n");
+        printf(RED"dot(a, b) failed\n"RESET);
     }
 
     Matrix *d = add(a, b);
     if (d->data[0][0] != 6 || d->data[0][1] != 8 || d->data[1][0] != 10 || d->data[1][1] != 12) {
-        printf("add(a, b) failed\n");
+        printf(RED"add(a, b) failed\n"RESET);
     }
 
     Matrix *e = multiply_s(a, 2);
     if (e->data[0][0] != 2 || e->data[0][1] != 4 || e->data[1][0] != 6 || e->data[1][1] != 8) {
-        printf("multiply_s(a, 2) failed\n");
+        printf(RED"multiply_s(a, 2) failed\n"RESET);
     }
 
     Matrix *f = power(a, 2);
     if (f->data[0][0] != 1 || f->data[0][1] != 4 || f->data[1][0] != 9 || f->data[1][1] != 16) {
-        printf("power(a, 2) failed\n");
+        printf(RED"power(a, 2) failed\n"RESET);
     }
 
     float g = sum(a);
     if (g != 10) {
-        printf("sum(a) failed\n");
+        printf(RED"sum(a) failed\n"RESET);
     }
 
     Matrix *h = transpose(a);
     if (h->data[0][0] != 1 || h->data[0][1] != 3 || h->data[1][0] != 2 || h->data[1][1] != 4) {
-        printf("transpose(a) failed\n");
+        printf(RED"transpose(a) failed\n"RESET);
     }
 
     Matrix *i = subtract(a, b);
     if (i->data[0][0] != -4 || i->data[0][1] != -4 || i->data[1][0] != -4 || i->data[1][1] != -4) {
-        printf("subtract(a, b) failed\n");
+        printf(RED"subtract(a, b) failed\n"RESET);
     }
 
     Matrix *j = multiply(a, b);
     if (j->data[0][0] != 5 || j->data[0][1] != 12 || j->data[1][0] != 21 || j->data[1][1] != 32) {
-        printf("multiply(a, b) failed\n");
+        printf(RED"multiply(a, b) failed\n"RESET);
     }
 
     destroy_matrix(a);
@@ -123,7 +126,7 @@ void test_nn_functions() {
     printf("output: %f %f\n", output->data[0][0], output->data[0][1]);
 
     if (output->data[0][0] - expected->data[0][0] > 0.1) {
-        printf("Training failed\n");
+        printf(RED"Training failed\n"RESET);
     }
 
     destroy_matrix(input);
