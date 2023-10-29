e7d10b2d7422b774e2d40e455be75d42bbda25d3
left <107749872+0xleft@users.noreply.github.com>
2023-10-29 21:13:46 +0100

    add or and xor

--- logic.c
+++ logic.c
@@ -41,7 +41,86 @@ void and() {
     destroy_dataset(dataset);
 }
 
+void or() {
+    Network *network;
+    int layer_sizes[] = {2, 1};
+    Activation activations[] = {SIGMOID};
+    network = create_network(1, layer_sizes, activations);
+
+    srand(66);
+    initialize_weights_xavier_norm(network);
+
+    Matrix *input1 = create_matrix_from_array(1, 2, (float[]) {0, 0});
+    Matrix *expected1 = create_matrix_from_array(1, 1, (float[]) {0});
+
+    Matrix *input2 = create_matrix_from_array(1, 2, (float[]) {0, 1});
+    Matrix *expected2 = create_matrix_from_array(1, 1, (float[]) {1});
+
+    Matrix *input3 = create_matrix_from_array(1, 2, (float[]) {1, 0});
+    Matrix *expected3 = create_matrix_from_array(1, 1, (float[]) {1});
+
+    Matrix *input4 = create_matrix_from_array(1, 2, (float[]) {1, 1});
+    Matrix *expected4 = create_matrix_from_array(1, 1, (float[]) {1});
+
+    Dataset *dataset = create_dataset(4);
+    add_data(dataset, input1, expected1);
+    add_data(dataset, input2, expected2);
+    add_data(dataset, input3, expected3);
+    add_data(dataset, input4, expected4);
+
+    // print_neural_network(network);
+
+    train_dataset(network, dataset, 10000, 1.0f);
+
+    // print_neural_network(network);
+
+    destroy_network(network);
+    destroy_dataset(dataset);
+}
+
+void xor() {
+    Network *network;
+    int layer_sizes[] = {2, 2, 1};
+    Activation activations[] = {SIGMOID, SIGMOID};
+    network = create_network(2, layer_sizes, activations);
+
+    srand(66);
+    initialize_weights_xavier_norm(network);
+
+    Matrix *input1 = create_matrix_from_array(1, 2, (float[]) {0, 0});
+    Matrix *expected1 = create_matrix_from_array(1, 1, (float[]) {0});
+
+    Matrix *input2 = create_matrix_from_array(1, 2, (float[]) {0, 1});
+    Matrix *expected2 = create_matrix_from_array(1, 1, (float[]) {1});
+
+    Matrix *input3 = create_matrix_from_array(1, 2, (float[]) {1, 0});
+    Matrix *expected3 = create_matrix_from_array(1, 1, (float[]) {1});
+
+    Matrix *input4 = create_matrix_from_array(1, 2, (float[]) {1, 1});
+    Matrix *expected4 = create_matrix_from_array(1, 1, (float[]) {0});
+
+    Dataset *dataset = create_dataset(4);
+    add_data(dataset, input1, expected1);
+    add_data(dataset, input2, expected2);
+    add_data(dataset, input3, expected3);
+    add_data(dataset, input4, expected4);
+
+    // print_neural_network(network);
+
+    train_dataset(network, dataset, 10000, 1.0f);
+
+    // print_neural_network(network);
+
+    destroy_network(network);
+    destroy_dataset(dataset);
+}
+
 int main() {
+    printf("running and\n");
     and();
+    printf("running or\n");
+    or();
+    printf("running xor\n");
+    xor();
     return 0;
 }
