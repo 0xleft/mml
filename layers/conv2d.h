b364bd1208d2c424c3f8d61d155e2beb2c4084e7
left <107749872+0xleft@users.noreply.github.com>
2023-10-31 13:07:42 +0100

    add basic methods for conv2d

--- layers/conv2d.h
+++ layers/conv2d.h
@@ -23,3 +23,9 @@ struct Conv2DLayer {
 };
 
 typedef struct Conv2DLayer Conv2DLayer;
+
+Conv2DLayer *create_conv2d_layer(int stride, int padding, int kernel_size, int input_size, int output_size, Activation activation, float epsilon, float decay_rate);
+void destroy_conv2d_layer(Conv2DLayer *layer);
+Matrix *forward_conv2d(Conv2DLayer *layer, Matrix *input);
+Matrix *backward_conv2d(Conv2DLayer *layer, Matrix *loss_gradient);
+void update_conv2d(Conv2DLayer *layer, float learning_rate);
