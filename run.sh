c3d65893ca5e4e72464a18cb48ff134e81e6cc2d
left <107749872+0xleft@users.noreply.github.com>
2023-10-31 13:02:48 +0100

    better run.sh

--- run.sh
+++ run.sh
@@ -4,24 +4,11 @@ echo "Running..."
 
 cmake .
 
-if [ "$1" = "test" ]; then
-    cmake --build . --target test -- -j 4
-    ./test
-    exit 0
+if [ "$1" = "" ]; then
+  echo "No target selected"
+  exit 0
 fi
 
-if [ "$1" = "logic" ]; then
-    cmake --build . --target logic_gates -- -j 4
-    ./logic_gates
-    exit 0
-fi
-
-if [ "$1" = "conv" ]; then
-    cmake --build . --target conv -- -j 4
-    ./conv
-    exit 0
-fi
-
-cmake --build . --target mml -- -j 4
-
-./mml $@
+cmake --build . --target $1 -- -j 4
+./$1
+exit 0
